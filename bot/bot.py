e568569fd3014419226183725e66d54efbbef4c3
poipole <viniradoaj@gmail.com>
2025-02-01 10:52:18 -0300

    Consertei o bot

--- bot/bot.py
+++ bot/bot.py
@@ -6,8 +6,8 @@ import logging
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 import discord
 import logging.handlers
-from datetime import datetime
-now = datetime.now()
+from time import ctime
+now = ctime()
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 token = os.environ["DISCORD_TOKEN"]
@@ -34,29 +34,20 @@ async def on_ready():
     print(f'Logado como: {client.user}')
 
 # Comandos abaixo
-# >hello:. Manda um olá em inglês.
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('>hello'):
-        await message.channel.send('Hello!')
-# >time: Manda o tempo atual do sistema do servidor
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    # >time: Manda o tempo atual do sistema do servidor
     if message.content.startswith('>time'):
         await message.channel.send(now)
-
-# >help: Manda uma mensagem com os comandos que o bot tem
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    # >hello:. Manda um olá em inglês.
+    if message.content.startswith('>hello'):
+        await message.channel.send("Hello!")
+    # >help: Manda todos os comandos do bot e explica o que eles fazem
     if message.content.startswith('>help'):
         await message.channel.send(">hello - Prints an hello."
+
                                    ">time - Tells the time")
 # Inicia o bot
 client.run(token, log_handler=None)
